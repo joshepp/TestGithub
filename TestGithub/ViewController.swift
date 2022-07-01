040be19d866fd1343cda7f0f867d5829ed8a7b5a
Jose  Alberto Martinez Pineda <jmartinezpineda4056@conestogac.on.ca>
2022-07-01 12:29:41 -0700

    Initial Commit

---
+++ TestGithub/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestGithub
+//
+//  Created by Jose Alberto Martinez Pineda on 2022-07-01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
